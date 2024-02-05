731071423b3bfbf99edc33921b8ceb6a4d6283b7
rajabu shabani <rajabujshabani@gmail.com>
2024-02-05 12:11:27 +0300

    conifgure dotenv and morgan

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+const morgan = require('morgan')
 const express = require('express')
-
 const app = express()
+const dotenv= require('dotenv')
+dotenv.config()
+
+if ( process.env.NODE_ENV === 'development') {
+   app.use(morgan('dev'))
+}
+// Accept json
+app.use(express.json())
 
 app.get('/home', (req, res) => {
     return res.json({ msg: "Hello raj"})
