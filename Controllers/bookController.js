2042edb741e8b9654d7bc372de59907cd698cc7d
rajabu shabani <rajabujshabani@gmail.com>
2024-02-07 23:52:23 +0300

    update book controller

--- Controllers/bookController.js
+++ Controllers/bookController.js
@@ -1,3 +1,10 @@
 export const getBooks = async (req, res ) => {
     res.status(200).json({ msg: 'success'})
 }
+export const createBook = async (req, res ) => {
+
+}
+
+export const updateBook = async (req, res ) => {
+
+}
