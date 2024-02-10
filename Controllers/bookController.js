23b450f861bddbbd772ea20920093b80df93286a
rajabu shabani <rajabujshabani@gmail.com>
2024-02-10 13:34:24 +0300

    add update book controller

--- Controllers/bookController.js
+++ Controllers/bookController.js
@@ -11,7 +11,14 @@ export const createBook = async (req, res ) => {
 }
 
 export const updateBook = async (req, res ) => {
-
+    const {id} = req.params
+    const updatedBook = await Book.findByIdAndUpdate(id, req.body, {
+        new: true
+    })
+    if (!updatedBook ) {
+        return res.status(400).json({ msg: `no book with id: ${id }`})
+    }
+    res.status(200).json({ updatedBook })
 }
 
 export const deleteBook = async (req, res ) => {
@@ -21,6 +28,7 @@ export const deleteBook = async (req, res ) => {
     if (!removedJob) {
         return res.status(404).json({ msg: `no book with id: ${id}`})
     }
+    res.status(200).json({ removedJob })
 }
 
 export const getBook = async (req, res ) => {
