a55b4f25076167872b20282dd8b9260edfdb70ca
rajabujshabani <rajabujshabani@gmail.com>
2024-02-15 17:47:30 +0300

    add functionality in express validator

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express()
 import dotenv from 'dotenv'
 dotenv.config()
 import { connectDB } from "./Config/Db.js"
+import { body , validationResult } from "express-validator";
 
 // Routers
 import bookRouter from "./Routers/bookRouters.js"
@@ -18,12 +19,26 @@ if ( process.env.NODE_ENV === 'development') {
 // Accept json
 app.use(express.json())
 
-app.use('/api/v1/books' , bookRouter )
 
-app.post('api/v1/test', (req, res) => {
+app.post(
+    '/api/v1/books/test',
+    [ body('name').notEmpty().withMessage('name is required')],
+    (req, res,next ) => {
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+          const errorMessage = errors.array().map(error => error.msg)
+          return res.status(400).json({ msg: errorMessage})
+      }
+        next();
+    },
+    (req, res) => {
     const { name } = req.body;
     res.json({ msg: `Hello ${name}`})
 })
+
+app.use('/api/v1/books' , bookRouter )
+
+
 app.use('*', (req, res) => {
     res.status(404).json({ msg: 'Not Found' })
 })
