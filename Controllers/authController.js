309e3df98effbd91c351415ac07d8d805f90163b
rajabujshabani <rajabujshabani@gmail.com>
2024-02-18 15:54:07 +0300

    add admin functionality in register controller

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,6 +1,9 @@
 import User from "../Models/userModel.js";
 import {StatusCodes} from "http-status-codes";
 export const register = async (req, res ) => {
+    const isFirstAccount = (await User.countDocuments()) === 0;
+    req.body.role = isFirstAccount ? 'admin' : 'author'
+
     const user = await User.create(req.body)
     res.status(StatusCodes.CREATED).json({ user })
 }
