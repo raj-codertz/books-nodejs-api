b84656afdc0ee5cff3d0b069fc329ef3d9a364d1
rajabujshabani <rajabujshabani@gmail.com>
2024-02-18 11:30:10 +0300

    add create method for user registeration

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,6 +1,8 @@
 import User from "../Models/userModel.js";
+import {StatusCodes} from "http-status-codes";
 export const register = async (req, res ) => {
-    const user = await User.create(req.body )
+    const user = await User.create(req.body)
+    res.status(StatusCodes.CREATED).json({ user })
 }
 
 export const login = async (req, res ) => {
