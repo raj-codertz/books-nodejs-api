143a1da3c62f905af8208a1e297772e8ecb3a2c2
rajabujshabani <rajabujshabani@gmail.com>
2024-02-21 16:03:48 +0300

    add http only cookie

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -18,15 +18,15 @@ export const login = async (req, res ) => {
     const user = await User.findOne({ email: req.body.email })
     const isValid = user && ( await comparePassword(req.body.password, user.password ))
     if (!isValid) throw new UnauthenticatedError('Invalid credentials')
-    // if (!user) throw new UnauthenticatedError('Invalid credentials')
-    //
-    // const isPasswordCorrect = await comparePassword(
-    //     req.body.password,
-    //     user.password
-    // )
-    // if ( !isPasswordCorrect ) throw new UnauthenticatedError('Invalid credentials')
+
     const token = createJWT({ userId: user._id, role: user.role })
-    res.json({ token })
+    const oneDay = 1000 * 60 * 60 * 24;
+    res.cookie('my token', token, {
+        httpOnly: true,
+        expires: new Date( Date.now() + oneDay ),
+        secure: process.env.NODE_ENV === 'production'
+    })
+    res.status(StatusCodes.OK).json({ msg: 'user login'})
 
 }
 
