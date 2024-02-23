f30ba6bf7c07f3c0b4a0462ba328eea0acf0b8b0
rajabujshabani <rajabujshabani@gmail.com>
2024-02-23 10:22:43 +0300

    token verification

--- Middleware/authMiddleware.js
+++ Middleware/authMiddleware.js
@@ -1,8 +1,15 @@
 import {UnauthenticatedError} from "../Errors/customErrors.js";
+import {verifyJWT} from "../Utils/tokenUtils.js";
+
 export const authenticateUser = (req, res, next ) => {
     const { my_token } = req.cookies
     if (!my_token) {
-        throw new UnauthenticatedError('Invalid credentials')
+        throw new UnauthenticatedError('authentication invalid')
     }
-    next()
+    try{
+        const { userId, role } = verifyJWT(my_token)
+        next()
+    } catch (e) {
+        throw new UnauthenticatedError('authentication invalid')
+    }
 }
